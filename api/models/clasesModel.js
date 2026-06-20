e740370cb747c0461560b234ac69ea6b801ec4dc
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2026-06-20 19:47:49 -0300

    Pongo todo en camel case

--- api/models/clasesModel.js
+++ api/models/clasesModel.js
@@ -1,23 +1,27 @@
 const { sequelize } = require('../config/db.js');
 const { DataTypes } = require('sequelize');
 
-const Clases = sequelize.define ('Clases',{
-    id_clase :{
-        type : DataTypes.INTEGER,
-        autoIncrement : true,
-        primaryKey : true
+const clases = sequelize.define('clases', {
+    id_clase: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
     },
-    personas : {
-        type : DataTypes.STRING,
-        allowNull : false
+    personas: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    es_profesor : {
-        type : DataTypes.BOOLEAN,
-        allowNull : false
+    es_profesor: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
     },
-    id_usuario : {
-        type : DataTypes.INTEGER,
-        allowNull : false
+    id_usuario: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     }
+
+}, {
+    tableName: 'clases',
+    timestamps: false
 });
-module.exports = {Clases};
+module.exports = { clases };
